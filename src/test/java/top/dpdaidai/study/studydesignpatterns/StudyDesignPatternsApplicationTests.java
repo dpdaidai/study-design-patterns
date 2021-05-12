7b4fcd8eff03993842a255e351a1f98ea8ab437c
cpt at mac <pantaochen@gmail.com>
2021-05-12 20:23:23 +0800

    初始化仓库

---
+++ src/test/java/top/dpdaidai/study/studydesignpatterns/StudyDesignPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package top.dpdaidai.study.studydesignpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDesignPatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
